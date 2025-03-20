7b071aa76da33f25295ea310e8ee8a19b963bad2
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-03-20 22:39:29 +0100

    feat(orders): integrate NotificationsModule and RedisCacheModule into OrdersModule
    - Added NotificationsModule to handle notification functionalities related to orders.
    - Integrated RedisCacheModule for enhanced caching capabilities within the OrdersModule.
    - Updated module imports to include new dependencies for improved order processing and notification handling.

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -5,11 +5,13 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RedisCacheModule } from '../cache/cache.module';
 
 @Module({
-	imports: [PrismaModule],
-	controllers: [OrdersController],
-	providers: [OrdersService],
-	exports: [OrdersService],
+  imports: [PrismaModule, NotificationsModule, RedisCacheModule],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+  exports: [OrdersService],
 })
-export class OrdersModule { } 
+export class OrdersModule {}
